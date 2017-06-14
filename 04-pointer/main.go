7eb9659d78d600c1d484057a428ad941c2020de9
Andreas Siegel <mail@andreassiegel.de>
2017-06-14 20:59:20 +0200

    some more stuff with pointers in a function

--- 04-pointer/main.go
+++ 04-pointer/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+func doStuff(x int) {
+
+	fmt.Printf("Value of argument x: %v \n", x)
+	fmt.Printf("Memory address of x: %v \n", &x)
+
+	x = x * 5
+
+	fmt.Printf("Value of argument x: %v \n", x)
+	fmt.Printf("Memory address of x: %v \n", &x)
+}
+
+func doStuffWithPointer(x *int) {
+	fmt.Printf("Value of argument x: %v \n", x)
+	fmt.Printf("Memory address of x: %v \n", &x)
+	fmt.Printf("Dereferenced value of x: %v \n", *x)
+
+	*x = *x * 5
+
+	fmt.Printf("Value of argument x: %v \n", x)
+	fmt.Printf("Memory address of x: %v \n", &x)
+	fmt.Printf("Dereferenced value of x: %v \n", *x)
+}
+
 func main() {
 	a := 43
 
@@ -14,5 +37,13 @@ func main() {
 	fmt.Printf("Dereferenced Value %v \n", *b)
 
 	*b = 42
+	fmt.Println(a)
+
+	doStuff(a)
+
+	fmt.Println(a)
+
+	doStuffWithPointer(&a)
+
 	fmt.Println(a)
 }
